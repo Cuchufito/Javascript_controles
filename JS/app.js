1074bf94d03f709479315acca3df32dcfb5013df
Cuchufito <nahuellugo123@gmail.com>
2024-07-20 19:36:59 -0400

    Commit

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+// DOMContenloaded sirve para controlar que primero se cargue TODO el documento
+// HTML, por ultimo Javascript
+document.
+addEventListener("DOMContentLoaded", function(){
+    alert("Holis");
+
+});
+
+function hola(nombre, apellido){
+    console.log(nombre, apellido);
+        //codigo reutilizable
+}
