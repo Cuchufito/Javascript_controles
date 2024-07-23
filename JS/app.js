cbc0d8bf98cbd8eba590bcc0899dd5203051d1ec
Cuchufito <nahuellugo123@gmail.com>
2024-07-22 20:01:10 -0400

    Actualizacion

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,32 @@
-// DOMContenloaded sirve para controlar que primero se cargue TODO el documento
-// HTML, por ultimo Javascript
-document.
-addEventListener("DOMContentLoaded", function(){
-    alert("Holis");
+/*
+DOMContentLoaded sirve para controlar que primero se cargue TODO el documento
+HTML, por ultimo javascript
+*/
+
+
+document
+.addEventListener("DOMContentLoaded", function(){
+//esta es zona segura
+ //console.log(document.getElementById("txtNombre").value);
+ //console.log(document.getElementById("txtApellido").value);
 
 });
 
+//añadiendo evento clic al boton mostrar
+const btnMostrar = document.getElementById("btnMostrar");
+btnMostrar.addEventListener("click", function(){
+    //console.log("haciendo clic");
+    //alert("hice clic");
+    //recuperando info del formulario
+    const nombres = document.getElementById("txtNombre").value;
+    const apellido = document.getElementById("txtApellido").value;
+    const cedula = document.getElementById("txtCedula").value;
+    const direccion = document.getElementById("txtDireccion").value;
+    console.log('nombre' + nombres + ', apellidos' + apellido +  ', cedula' + cedula + ', direccion' + direccion);
+})
+
+
 function hola(nombre, apellido){
-    console.log(nombre, apellido);
-        //codigo reutilizable
-}
+console.log(nombre, apellido);
+//codigo reutilizable
+}   
